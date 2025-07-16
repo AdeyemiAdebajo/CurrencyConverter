99f9c05a3699b5001dd85e587fa5c1353308d214
Adeyemi Adebajo <127756450+AdeyemiAdebajo@users.noreply.github.com>
2025-07-16 09:38:10 -0400

    conversion logic completed

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 import express from "express";
+import bodyParser from "body-parser";
 const app = express();
 
 const port=3000;
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
 });
 
+app.post("/convert", (req, res) => {
+    const frmcurrency = req.body["fromCurrency"];
+    const tocurrency = req.body["toCurrency"];
+    const amount = req.body["amount"];
+    const exchangeRate = 1.2; 
+    const convertedAmount = (req.body["amount"]* exchangeRate).toFixed(2);
+    
+    console.log(`From: ${frmcurrency}, To: ${tocurrency}, Amount: ${amount}`);
+    res.render("index.ejs", {
+        moneyConverted: convertedAmount,
+        fromCurrency: frmcurrency,
+        toCurrency: tocurrency,
+        amount: amount,
+        result: `Converted ${amount} ${frmcurrency} to ${tocurrency}`
+    });
+});
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
